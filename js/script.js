c7d6930c9a68e425c55e7b1e4a66b2d6224c774e
Hanna Warońska <waronskahania@gmail.com>
2024-06-17 16:29:10 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let currencyElement = document.querySelector(".js-currency");
+let howMuchElement = document.querySelector(".js-howMuch");
+let changed = document.querySelector(".js-changed");
+let form = document.querySelector(".form");
+
+form.addEventListener("input", () => {
+    let currency = parseFloat(currencyElement.value);
+    let howMuch = +howMuchElement.value;
+    let now = howMuch / currency;
+    let walue;
+
+    switch (currency) {
+        case 4.3605:
+            walue = "EUR"
+            break;
+        case 4.0718:
+            walue = "USD"
+            break;
+        case 5.1733:
+            walue = "USD"
+            break;
+        case 4.5714:
+            walue = "USD"
+            break;
+    }
+
+    changed.innerHTML = `${howMuch.toFixed(2)} PLN = <strong>${now.toFixed(2)} ${walue}</strong>`;
+})
