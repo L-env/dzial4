82a57f3bb0e8b162b552be3167d668cc5d989d3d
Hanna Warońska <waronskahania@gmail.com>
2024-06-20 20:06:33 +0200

    przekszatalcenie funkcji

--- js/script.js
+++ js/script.js
@@ -1,28 +1,45 @@
-let currencyElement = document.querySelector(".js-currency");
-let howMuchElement = document.querySelector(".js-howMuch");
-let changed = document.querySelector(".js-changed");
-let form = document.querySelector(".form");
+{
+    const calculateResult = (currency) => {
 
-form.addEventListener("input", () => {
-    let currency = parseFloat(currencyElement.value);
-    let howMuch = +howMuchElement.value;
-    let now = howMuch / currency;
-    let walue;
+        switch (currency) {
+            case 4.3605:
+                return "EUR"
 
-    switch (currency) {
-        case 4.3605:
-            walue = "EUR"
-            break;
-        case 4.0718:
-            walue = "USD"
-            break;
-        case 5.1733:
-            walue = "USD"
-            break;
-        case 4.5714:
-            walue = "USD"
-            break;
+            case 4.0718:
+                return "USD"
+
+            case 5.1733:
+                return "USD"
+
+            case 4.5714:
+                return "USD"
+        }
     }
 
-    changed.innerHTML = `${howMuch.toFixed(2)} PLN = <strong>${now.toFixed(2)} ${walue}</strong>`;
-})
+    const updateResultText = (howMuch, now, walue) => {
+        const changed = document.querySelector(".js-changed");
+        changed.innerHTML = `${howMuch.toFixed(2)} PLN = <strong>${now.toFixed(2)} ${walue}</strong>`;
+    }
+
+    const onFormSubmit = () => {
+        const currencyElement = document.querySelector(".js-currency");
+        const howMuchElement = document.querySelector(".js-howMuch");
+
+        const currency = parseFloat(currencyElement.value);
+        const howMuch = +howMuchElement.value;
+        const now = howMuch / currency;
+
+        const walue = calculateResult(currency)
+
+        updateResultText(howMuch, now, walue);
+    }
+
+    const init = () => {
+
+        const form = document.querySelector(".form");
+
+        form.addEventListener("input", onFormSubmit);
+    }
+
+    init();
+}
